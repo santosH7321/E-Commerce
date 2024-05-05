9a8d68813bc4160bddde13a1144490c94d748520
Santosh Kumar <santoshkumar23kky@gmail.com>
2024-05-05 11:49:36 +0530

    folderAndRoutes

---
+++ frontend/src/components/Header.js
@@ -0,0 +1,10 @@
+
+const Header = () => {
+    return(
+        <div className="flex justify-center items-center text-2xl font-bold text-blue-700">
+            <h1>This is header file</h1>
+        </div>
+    )
+}
+
+export default Header;
---
+++ frontend/src/pages/Home.js
@@ -0,0 +1,11 @@
+
+
+const Home = () => {
+    return(
+        <div className="flex justify-center items-center text-2xl font-bold text-sky-700">
+            <h1>This is home page 😃</h1>
+        </div>
+    )
+}
+
+export default Home;
