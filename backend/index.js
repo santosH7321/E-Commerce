0b6436ada59f22ed79a8e01d161963bd6c7921ea
Santosh Kumar <santoshkumar23kky@gmail.com>
2024-05-06 16:54:31 +0530

    signUp

--- backend/index.js
+++ backend/index.js
@@ -1,12 +1,19 @@
 const express = require('express')
 const cors = require('cors')
 require('dotenv').config()
+const connectDB = require('./config/db')
+const router = require('./routes')
 
 const app = express()
 app.use(cors())
+app.use("/api", router)
 
 const PORT = 8080 || process.env.PORT
 
-app.listen(PORT,() => {
-    console.log("Server is running")
+connectDB().then(() => {
+    app.listen(PORT,() => {
+        console.log("Connected to DB")
+        console.log("Server is running")
+    })
 })
+
