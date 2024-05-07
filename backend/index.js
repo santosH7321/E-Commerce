7a0ae0bf60e9329e10fd04f98f5dbacd64d548b3
Santosh Kumar <santoshkumar23kky@gmail.com>
2024-05-07 08:04:18 +0530

    imageUpload

--- backend/index.js
+++ backend/index.js
@@ -1,13 +1,20 @@
 const express = require('express')
 const cors = require('cors')
 require('dotenv').config()
+const cookieParser = require('cookie-parser')
 const connectDB = require('./config/db')
 const router = require('./routes')
 
 const app = express()
-app.use(cors())
+app.use(cors(
+    {
+    origin : process.env.FRONTEND_URL,
+    Credential : true
+    }
+))
 app.use(express.json())
 app.use("/api", router)
+app.use(cookieParser())
 
 const PORT = 8080 || process.env.PORT
 
